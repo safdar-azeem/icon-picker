ee1e83c7330175d66e8b24d818a142994ae48a11
safdar-azeem <safdarazeem00@gmail.com>
2025-05-19 01:00:18 +0500

    Add App.vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import IconPicker from './IconPicker.vue'
+import { ref } from 'vue'
+
+const icon = ref('')
+</script>
+
+<template>
+	<div>
+		<IconPicker v-model="icon" />
+	</div>
+</template>
+
+<style>
+div {
+	max-width: 200px;
+}
+</style>
